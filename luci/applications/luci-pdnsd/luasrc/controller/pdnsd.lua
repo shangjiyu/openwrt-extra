0c677ed76c949b8e26752af1796aaec2a14ed74c
shangjiyu <shangjiyu@gmail.com>
2014-07-10 15:33:01 +0800

    change entry dir test

--- luci/applications/luci-pdnsd/luasrc/controller/pdnsd.lua
+++ luci/applications/luci-pdnsd/luasrc/controller/pdnsd.lua
@@ -6,12 +6,12 @@ module("luci.controller.pdnsd", package.seeall)
 
 function index()
 	
-	if not nixio.fs.access("/etc/pdnsd.conf") then
+	--[[if not nixio.fs.access("/etc/pdnsd.conf") then
 		return
-	end
+	end]]--
 
 	local page
-	page = entry({"admin", "RA-MOD", "pdnsd"}, cbi("pdnsd"), _("pdnsd"), 35)
+	page = entry({"admin", "services", "pdnsd"}, cbi("pdnsd"), _("pdnsd"), 35)
 	page.i18n = "pdnsd"
 	page.dependent = true
 end
--- luci/applications/luci-redsocks2/luasrc/controller/redsocks2.lua
+++ luci/applications/luci-redsocks2/luasrc/controller/redsocks2.lua
@@ -7,7 +7,7 @@ module("luci.controller.redsocks2", package.seeall)
 function index()
 
 	local page
-	page = entry({"admin", "RA-MOD", "redsocks2"}, cbi("redsocks2"), _("redsocks2"), 50)
+	page = entry({"admin", "services", "redsocks2"}, cbi("redsocks2"), _("redsocks2"), 50)
 	page.i18n = "redsocks2"
 	page.dependent = true
 end
