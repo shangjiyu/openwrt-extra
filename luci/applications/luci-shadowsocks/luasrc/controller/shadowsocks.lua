e38cbed0821f7dd85a12e834730ab76e0ce9e1c6
shangjiyu <shangjiyu@gmail.com>
2014-12-11 15:33:04 +0800

    fix service title

--- luci/applications/luci-shadowsocks/luasrc/controller/shadowsocks.lua
+++ luci/applications/luci-shadowsocks/luasrc/controller/shadowsocks.lua
@@ -11,11 +11,6 @@ function index()
 	end
 
 	local page
-	page = node("admin", "services")
-	page.target = firstchild()
-	page.title = _("services")
-	page.order  = 65
-
 	page = entry({"admin", "services", "shadowsocks"}, cbi("shadowsocks"), _("shadowsocks"), 45)
 	page.i18n = "shadowsocks"
 	page.dependent = true
