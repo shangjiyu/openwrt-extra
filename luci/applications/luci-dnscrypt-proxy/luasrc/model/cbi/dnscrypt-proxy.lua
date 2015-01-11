c04fdb043462657096afbdc81f214bfbde558ccc
shangjiyu <shangjiyu@gmail.com>
2015-01-11 17:54:34 +0800

    fix again

--- luci/applications/luci-dnscrypt-proxy/luasrc/model/cbi/dnscrypt-proxy.lua
+++ luci/applications/luci-dnscrypt-proxy/luasrc/model/cbi/dnscrypt-proxy.lua
@@ -17,9 +17,13 @@ s.anonymous = true
 switch = s:option(Flag, "enabled", translate("Enable"))
 switch.rmempty = false
 
-upstream = s:option(Value, "local_address", translate("Listenning Address:Port"))
+upstream = s:option(Value, "resolver_name", translate("DNSCrypt-Proxy Server"))
 upstream.optional = false
-upstream.default = "127.0.0.1:5300"
+upstream.default = "opendns"
+
+local_address = s:option(Value, "local_address", translate("Listenning Address:Port"))
+local_address.optional = false
+local_address.default = "127.0.0.1:5300"
 
 MaxActReq = s:option(Value, "max_active_requests", translate("Max Active Request"))
 MaxActReq.optional = false
