5549f35e8415431a35f24927b2e0b6e05ef95d72
shangjiyu <shangjiyu@gmail.com>
2015-04-27 10:28:23 +0800

    add network check

--- packages/drcom/src/drmonitor.sh
+++ packages/drcom/src/drmonitor.sh
@@ -2,13 +2,25 @@
 
 isfound=$(ps | grep "drcom" | grep -v "grep");
 
-if [ -z "$isfound" -o `date +%H%M` = '810' -o `date +%H%M` = '0810' ]; then
-        echo "$(date): start drcom...">>/tmp/log/drcom-monitor.log;
-        killall drcom >/dev/null 2>&1;
-        [ -f '/var/run/drcom.pid' ] && {
-                rm -f /var/run/drcom.pid >/dev/null;
-        }
-        uci set drcom.config.enabled=1;
-        uci commit drcom;
-        /etc/init.d/drcom restart >/dev/null 2>&1;
+connected=0
+
+for url in www.baidu.com www.so.com ; do
+    result=$(curl -o /dev/null -s -m 10 -w %{http_code} $url)
+        for flag in 200 301 302 404 ; do
+            if [ $flag = $result ];then
+                connected=$(expr $connected + 1)
+                echo "connected to $url"
+            fi
+        done
+done
+
+if [ $connected -eq 0 -o -z "$isfound" -o `date +%H%M` = '810' -o `date +%H%M` = '0810' ]; then
+    echo "$(date): start drcom...">>/tmp/log/drcom-monitor.log;
+    killall drcom >/dev/null 2>&1;
+    [ -f '/var/run/drcom.pid' ] && {
+        rm -f /var/run/drcom.pid >/dev/null;
+    }
+    uci set drcom.config.enabled=1;
+    uci commit drcom;
+    /etc/init.d/drcom restart >/dev/null 2>&1;
 fi
