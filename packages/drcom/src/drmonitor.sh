268372d5223e26be82360353b091263af92e84fc
shangjiyu <shangjiyu@gmail.com>
2015-05-27 21:06:44 +0800

    update drcom monitor script

--- packages/drcom/src/drmonitor.sh
+++ packages/drcom/src/drmonitor.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 isfound=$(ps | grep "drcom" | grep -v "grep");
-
-connected=0
-
-for url in www.baidu.com www.so.com ; do
-    result=$(curl -o /dev/null -s -m 10 -w %{http_code} $url)
-        for flag in 200 301 302 404 ; do
-            if [ $flag = $result ];then
-                connected=$(expr $connected + 1)
-                echo "connected to $url"
-            fi
+isCurl=$(which curl)
+if [ -z "$isCurl" ];then
+        echo 'no curl'
+        connected=2
+else
+        connected=0
+        for url in www.baidu.com www.so.com ; do
+                result=$(curl -o /dev/null -s -m 10 -w %{http_code} $url)
+                for flag in 200 301 302 404 ; do
+                        if [ $flag = $result ];then
+                                connected=$(expr $connected + 1)
+                                echo "connected to $url"
+                        fi
+                done
         done
-done
-
+fi
 if [ $connected -eq 0 -o -z "$isfound" -o `date +%H%M` = '810' -o `date +%H%M` = '0810' ]; then
     echo "$(date): start drcom...">>/tmp/log/drcom-monitor.log;
     killall drcom >/dev/null 2>&1;
