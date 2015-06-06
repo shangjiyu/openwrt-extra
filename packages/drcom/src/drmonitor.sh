96fbc89884a241030ad03fa34892cd2ea4179719
shangjiyu <shangjiyu@gmail.com>
2015-06-06 12:19:53 +0800

    fix dr monitor script

--- packages/drcom/src/drmonitor.sh
+++ packages/drcom/src/drmonitor.sh
@@ -4,12 +4,12 @@ isfound=$(ps | grep "drcom" | grep -v "grep");
 isCurl=$(which curl)
 if [ -z "$isCurl" ];then
         echo 'no curl'
-        connected=2
+        connected='No CURL Found'
 else
         connected=0
         for url in www.baidu.com www.so.com ; do
                 result=$(curl -o /dev/null -s -m 10 -w %{http_code} $url)
-                for flag in 200 301 302 404 ; do
+                for flag in 200 301 404 ; do
                         if [ $flag = $result ];then
                                 connected=$(expr $connected + 1)
                                 echo "connected to $url"
@@ -18,7 +18,7 @@ else
         done
 fi
 if [ $connected -eq 0 -o -z "$isfound" -o `date +%H%M` = '810' -o `date +%H%M` = '0810' ]; then
-    echo "$(date): start drcom...">>/tmp/log/drcom-monitor.log;
+    echo "$(date): start drcom(connected: $connected)...">>/tmp/log/drcom-monitor.log;
     killall drcom >/dev/null 2>&1;
     [ -f '/var/run/drcom.pid' ] && {
         rm -f /var/run/drcom.pid >/dev/null;
