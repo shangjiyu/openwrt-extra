15ed304b49ea6e5318aa66d5d6fa105d5ab7c871
shangjiyu <shangjiyu@gmail.com>
2016-04-28 16:48:50 +0800

    fix script

--- packages/glorytun/files/glorytun.sh
+++ packages/glorytun/files/glorytun.sh
@@ -24,7 +24,7 @@ if glorytun version | grep mud ; then
     config_load network
     config_foreach add_multipath interface
 else
-    GLORYTUN_ARGS="retry count -1 const 5000000 timeout 5000 keepalive count 3 idle 10 interval 1 mptcp"
+    GLORYTUN_ARGS="retry count -1 const 5000000 timeout 5000 keepalive count 3 idle 10 interval 1"
 fi
 
 trap "pkill -TERM -P $$" TERM
@@ -36,10 +36,10 @@ initialized() {
     [ -n "${GLORYTUN_MTU}" ] && ip link set ${GLORYTUN_DEV} mtu ${GLORYTUN_MTU}
     [ -n "${GLORYTUN_TXQLEN}" ] && ip link set ${GLORYTUN_DEV} txqueuelen ${GLORYTUN_TXQLEN}
 
-    # Workaround to make mwan3 update tun rule
-    /etc/init.d/sqm start ${GLORYTUN_DEV}
-
-    multipath ${GLORYTUN_DEV} off
+#    # Workaround to make mwan3 update tun rule
+#    /etc/init.d/sqm start ${GLORYTUN_DEV}
+#
+#    multipath ${GLORYTUN_DEV} off
 }
 
 started() {
@@ -56,13 +56,13 @@ started() {
 
     ubus call network.interface.${GLORYTUN_DEV} up
 
-    if [ "${GLORYTUN_DEV}" == "tun0" ]; then
-        [ -x /etc/init.d/shadowsocks ] && /etc/init.d/shadowsocks start;
-    fi
+#    if [ "${GLORYTUN_DEV}" == "tun1" ]; then
+#        [ -x /etc/init.d/shadowsocks ] && /etc/init.d/shadowsocks start;
+#    fi
 }
 
 stopped() {
-    if [ "${GLORYTUN_DEV}" == "tun0" ]; then
+    if [ "${GLORYTUN_DEV}" == "tun1" ]; then
         [ -x /etc/init.d/shadowsocks ] && /etc/init.d/shadowsocks stop;
     fi
 
